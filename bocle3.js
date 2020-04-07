ff2921aa1ed06e72fb575e61f233715b622e0331
lavidasinamor <kamal1994zariah@gmail.com>
2020-04-07 22:21:26 +0100

    Create bocle3.js

---
+++ bocle3.js
@@ -0,0 +1,8 @@
+ var perPage=8;
+    var numPages=6;
+    var firstText ='First';
+    var lastText ='Last';
+    var prevText ='� Previous';
+    var nextText ='Next �';
+    var urlactivepage=location.href;
+    var home_page="/";
