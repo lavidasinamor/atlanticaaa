a4b7cd60e1b86ba618408a193bc469bc1b2d7ed4
lavidasinamor <kamal1994zariah@gmail.com>
2020-04-07 22:19:18 +0100

    Create boclelikhasehatkon1.js

---
+++ boclelikhasehatkon1.js
@@ -0,0 +1,7 @@
+var no_image = "http://3.bp.blogspot.com/-Yw8BIuvwoSQ/VsjkCIMoltI/AAAAAAAAC4c/s55PW6xEKn0/s1600-r/nth.png";
+var month_format = [, "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
+var related_number = 3;
+var more_text = "View More";
+var comments_text = "<span>Post </span>Comment";
+var POSTPAGER_OLDER = "Older Article"; // post nav text "previous post"
+var POSTPAGER_NEWER = "Newer Article"; // post nav text "next post"
